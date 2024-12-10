70d300cc4dce8127cdfdb6e92f0e4b9db330d0b5
unknown <nabilsaputra25@gmail.com>
2024-12-10 15:06:28 +0800

    balikin

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,7 +1,5 @@
 @extends('dashboard.adminDashboard')
 
-<p>gg</p>
-
 @section('content')
     <div
         class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
