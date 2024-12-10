a9958155efbaa19352cfc63ca9e6037b3c9363ce
unknown <nabilsaputra25@gmail.com>
2024-12-10 15:11:38 +0800

    oke

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,5 +1,4 @@
 @extends('dashboard.adminDashboard')
-<p>tes</p>
 @section('content')
     <div
         class="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
