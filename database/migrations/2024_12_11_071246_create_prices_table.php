b4ed78e114f2d5af89c2793c96b7cd97e2402956
hamasasi12 <hamasakifsanie@gmail.com>
2024-12-12 09:10:37 +0800

    fixing prices migration

--- database/migrations/2024_12_11_071246_create_prices_table.php
+++ database/migrations/2024_12_11_071246_create_prices_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('regulation');
             $table->string('benefit');
             $table->string('discount');
-            $table->string('price');
             $table->timestamps();
         });
     }
