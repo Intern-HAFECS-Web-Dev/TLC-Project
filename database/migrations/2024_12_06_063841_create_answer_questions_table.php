bdff145ac8c72910f9cd389d3ddc5c778d189dee
Sabardi <Sabardibahari@gmail.com>
2024-12-25 13:46:22 +0700

    prever

--- database/migrations/2024_12_06_063841_create_answer_questions_table.php
+++ database/migrations/2024_12_06_063841_create_answer_questions_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('answer');
             $table->boolean('is_correct');
-            $table->integer('sesion_exam');
             $table->foreignId('question_id')->constrained('questions')->onDelete('cascade');
             $table->timestamps();
         });
